a968ce8a9c8e51969d08884ec2b21e08bf39666c
Yasmeenarh <yasmeenarh05@gmail.com>
2026-05-18 13:21:48 +0100

    Revert Dev D Work

--- client/src/guards/CollectorGuard.jsx
+++
@@ -1,5 +0,0 @@
-import ProtectedRoute from './ProtectedRoute';
-
-export default function CollectorGuard(props) {
-  return <ProtectedRoute allowedRoles={['collector']} loginPath="/collector/login" {...props} />;
-}
--- client/src/guards/UserGuard.jsx
+++
@@ -1,5 +0,0 @@
-import ProtectedRoute from './ProtectedRoute';
-
-export default function UserGuard(props) {
-  return <ProtectedRoute allowedRoles={['user']} loginPath="/login" {...props} />;
-}
